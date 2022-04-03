43b60f2ae3e345b13988f2c2607599efd120ee54
MrKren <krengaming@gmail.com>
2022-04-03 18:11:34 +0100

    Add theme

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#009688",
+    },
+    secondary: {
+      main: "#7cb342",
+    },
+  },
+});
 
 function App() {
-  return <div></div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <div>
+        <Button variant="contained">Test</Button>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
