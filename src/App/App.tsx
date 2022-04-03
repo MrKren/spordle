6317009dfedb061d539076c7a637c6423009bf00
MrKren <krengaming@gmail.com>
2022-04-03 23:05:22 +0100

    Add title

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
 
 const theme = createTheme({
   palette: {
@@ -16,9 +17,12 @@ const theme = createTheme({
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div>
+      <Container
+        sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+      >
+        <h1>Spotify Heardle</h1>
         <Button variant="contained">Test</Button>
-      </div>
+      </Container>
     </ThemeProvider>
   );
 }
