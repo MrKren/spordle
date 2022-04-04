6e4d8dfa2ce3aba81d1f7a8b123e8e04437112fd
MrKren <krengaming@gmail.com>
2022-04-04 19:57:21 +0100

    Add auth flow

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Container from "@mui/material/Container";
 import Selector from "../Components/Selector";
@@ -14,14 +14,38 @@ const theme = createTheme({
   },
 });
 
+const clientId = encodeURIComponent("cd8d92cbc0ea42fc8ad3e9b0997b1b8b");
+const redirectUri = encodeURIComponent("http://localhost:3000");
+let authUrl = `https://accounts.spotify.com/authorize`;
+authUrl += "?response_type=token";
+authUrl += `&client_id=${clientId}`;
+authUrl += `&redirect_uri=${redirectUri}`;
+
 function App() {
+  const [authenticated, setAuthenticated] = useState(false as boolean);
+  const [token, setToken] = useState(null as null | string);
+  const currentUrl = window.location.href;
+
+  useEffect(() => {
+    const index = currentUrl.indexOf("#") + 1;
+    const searchUrl = `${currentUrl.slice(0, index)}&${currentUrl.slice(
+      index
+    )}`;
+    const tokenSearch = new URLSearchParams(searchUrl);
+    if (tokenSearch.get("access_token")) {
+      setAuthenticated(true);
+      setToken(tokenSearch.get("access_token"));
+    }
+  }, [currentUrl]);
+
   return (
     <ThemeProvider theme={theme}>
       <Container
         sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
       >
         <h1>Spotify Heardle</h1>
-        <Selector />
+        {!authenticated && <a href={authUrl}>Authenticate</a>}
+        {authenticated && <Selector />}
       </Container>
     </ThemeProvider>
   );
