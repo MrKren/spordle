29c2cdd24ca43c00505869cc45d057afb5ada4e0
MrKren <krengaming@gmail.com>
2022-04-05 00:07:52 +0100

    Add playlist request logic

--- src/Components/Selector/Selector.tsx
+++ src/Components/Selector/Selector.tsx
@@ -1,29 +1,46 @@
-import React, { useState, VFC } from "react";
+import React, { useEffect, useState, VFC } from "react";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import { Token, SelectorProps } from "../types";
+import { SelectorProps } from "../types";
+import axios from "axios";
 
 const Selector: VFC<SelectorProps> = ({ token }) => {
+  const [textfieldValue, setTextfieldValue] = useState("" as string);
   const [playlistId, setPlaylistId] = useState("" as string);
   const apiUrl = "https://api.spotify.com/v1/playlists/";
 
-  const collectPlaylist = () => {
-    console.log(playlistId);
-  };
+  useEffect(() => {
+    // Process url
+    const returnUrl = "https://open.spotify.com/playlist/";
+    let id = playlistId;
+    if (id.startsWith(returnUrl)) {
+      id = id.replace(returnUrl, "");
+      id = id.slice(0, id.indexOf("?"));
+    }
+
+    if (id.length > 0) {
+      const playlistObj = axios.get(`${apiUrl}${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+    }
+  }, [playlistId, token]);
 
   return (
     <Box sx={{ display: "flex", gap: "10px" }}>
       <TextField
         label="Playlist ID"
-        onChange={(URI) => setPlaylistId(URI.target.value)}
+        onChange={(e) => setTextfieldValue(e.target.value)}
         onKeyPress={(e) => {
           if (e.key === "Enter") {
-            collectPlaylist();
+            setPlaylistId(textfieldValue);
           }
         }}
       />
-      <Button variant="contained" onClick={collectPlaylist}>
+      <Button variant="contained" onClick={() => setPlaylistId(textfieldValue)}>
         GO
       </Button>
     </Box>
