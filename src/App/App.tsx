f1a4eb991fb6c1d50fee81181b59783dca3aaaa1
Kieran Dominy <38659105+MrKren@users.noreply.github.com>
2022-04-06 07:49:00 +0100

    Create Selector component (#1)
    * Add title
    
    * Build basic playlist selector component
    
    * Add auth flow
    
    * Pass token as prop to selector
    
    * Add playlist request logic
    
    * Pass up playlist obj to App
    
    * Add error handling

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Selector from "../Components/Selector";
+import { Playlist, Token } from "../Components/types";
 
 const theme = createTheme({
   palette: {
@@ -13,12 +15,48 @@ const theme = createTheme({
   },
 });
 
+const clientId = encodeURIComponent("cd8d92cbc0ea42fc8ad3e9b0997b1b8b");
+const redirectUri = encodeURIComponent("http://localhost:3000");
+let authUrl = `https://accounts.spotify.com/authorize`;
+authUrl += "?response_type=token";
+authUrl += `&client_id=${clientId}`;
+authUrl += `&redirect_uri=${redirectUri}`;
+
 function App() {
+  //Authentication
+  const [authenticated, setAuthenticated] = useState(false as boolean);
+  const [token, setToken] = useState(null as Token);
+  const currentUrl = window.location.href;
+
+  useEffect(() => {
+    const index = currentUrl.indexOf("#") + 1;
+    const searchUrl = `${currentUrl.slice(0, index)}&${currentUrl.slice(
+      index
+    )}`;
+    const tokenSearch = new URLSearchParams(searchUrl);
+    const tokenValue = tokenSearch.get("access_token");
+    if (tokenValue) {
+      setAuthenticated(true);
+      setToken(tokenValue);
+    }
+  }, [currentUrl]);
+
+  //Main app logic
+  const [playlist, setPlaylist] = useState({} as Playlist);
+
+  useEffect(() => {
+    console.log(playlist);
+  }, [playlist]);
+
   return (
     <ThemeProvider theme={theme}>
-      <div>
-        <Button variant="contained">Test</Button>
-      </div>
+      <Container
+        sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+      >
+        <h1>Spotify Heardle</h1>
+        {!authenticated && <a href={authUrl}>Authenticate</a>}
+        {authenticated && <Selector token={token} setPlaylist={setPlaylist} />}
+      </Container>
     </ThemeProvider>
   );
 }
---
+++ src/Components/Selector/Selector.tsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState, VFC } from "react";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import { Playlist, SelectorProps } from "../types";
+import axios, { AxiosResponse } from "axios";
+import { FormHelperText } from "@mui/material";
+
+const Selector: VFC<SelectorProps> = ({ token, setPlaylist }) => {
+  const [textfieldValue, setTextfieldValue] = useState("" as string);
+  const [playlistId, setPlaylistId] = useState("" as string);
+  const [apiResponse, setApiResponse] = useState({
+    status: 200,
+  } as AxiosResponse);
+  const apiUrl = "https://api.spotify.com/v1/playlists/";
+
+  useEffect(() => {
+    // Process url
+    const returnUrl = "https://open.spotify.com/playlist/";
+    let id = playlistId;
+    if (id.startsWith(returnUrl)) {
+      id = id.replace(returnUrl, "");
+      id = id.slice(0, id.indexOf("?"));
+    }
+
+    // Request playlist data
+    if (id.length > 0) {
+      const playlistObj = axios
+        .get(`${apiUrl}${id}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        })
+        .then((response) => {
+          setApiResponse(response);
+          setPlaylist({ ...response.data } as Playlist);
+        })
+        .catch((error) => {
+          setApiResponse(error.response);
+        });
+    }
+  }, [playlistId, token, setPlaylist]);
+
+  const errorCode = () => {
+    if (apiResponse.status === 401) {
+      return "Session Expired";
+    }
+    if (apiResponse.status === 404) {
+      return "Playlist not found";
+    }
+  };
+
+  return (
+    <Box>
+      <Box sx={{ display: "flex", gap: "10px" }}>
+        <TextField
+          error={apiResponse.status !== 200}
+          label="Playlist ID/Link"
+          onChange={(e) => setTextfieldValue(e.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              setPlaylistId(textfieldValue);
+            }
+          }}
+        />
+        <Button
+          variant="contained"
+          onClick={() => setPlaylistId(textfieldValue)}
+        >
+          GO
+        </Button>
+      </Box>
+      <Box sx={{ marginLeft: "10px" }}>
+        <FormHelperText error>{errorCode()}</FormHelperText>
+      </Box>
+    </Box>
+  );
+};
+
+export default Selector;
