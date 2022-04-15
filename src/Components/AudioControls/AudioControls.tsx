f80f476e53bc409bab57f3311e4ec007585eb110
Kieran Dominy <38659105+MrKren@users.noreply.github.com>
2022-04-15 15:30:09 +0100

    Fix small bugs (#10)
    * Update README
    
    * Fix audio not loading on mobile
    
    * Add loading bar
    
    * Change metadata

--- src/Components/AudioControls/AudioControls.tsx
+++ src/Components/AudioControls/AudioControls.tsx
@@ -1,14 +1,17 @@
-import { Box, Fab } from "@mui/material";
+import { Box, CircularProgress, Fab } from "@mui/material";
 import React, { useEffect, useRef, useState, VFC } from "react";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import PauseIcon from "@mui/icons-material/Pause";
+import DownloadingIcon from "@mui/icons-material/Downloading";
 import { AudioControlsProps } from "../types";
 
 const AudioControls: VFC<AudioControlsProps> = ({ song, guessNum }) => {
   const [loaded, setLoaded] = useState(false);
   const [playing, setPlaying] = useState(false);
   const [time, setTime] = useState(1);
-  const audioRef = useRef(new Audio());
+  const [initialClick, setInitialClick] = useState(false);
+  const audioRef = useRef(new Audio(song.link));
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
   useEffect(() => {
     setTime((t) => t + guessNum + 1);
@@ -36,6 +39,22 @@ const AudioControls: VFC<AudioControlsProps> = ({ song, guessNum }) => {
       {loaded && (
         <Fab onClick={() => setPlaying(!playing)}>
           {playing ? <PauseIcon /> : <PlayArrowIcon />}
+        </Fab>
+      )}
+      {!isMobile && !loaded && (
+        <Fab>
+          <CircularProgress />
+        </Fab>
+      )}
+      {isMobile && !loaded && (
+        <Fab
+          onClick={() => {
+            setInitialClick(true);
+            audioRef.current.play(); // To trigger loading of track on IOS
+            audioRef.current.pause();
+          }}
+        >
+          {initialClick ? <CircularProgress /> : <DownloadingIcon />}
         </Fab>
       )}
       <Box sx={{ marginTop: "10px" }}>
